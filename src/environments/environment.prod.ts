60e86d701e565a8075a674d9e8046a425021065e
DannePeyya <danne@peyya.co>
2022-10-09 02:47:59 +0200

    Updating prod envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     clientId: '18KmUu5qurxczk50GaZ3eCxQy8zD5H5O',
     redirectUri: window.location.origin,
     // Request this audience at user authentication time
-    audience: 'https://dev-ibqj5g5m.us.auth0.com/api/v2/',
+    audience: 'https://peyya.api',
 
     // Request this scope at user authentication time
     scope: 'read:current_user read:email',
@@ -16,15 +16,26 @@ export const environment = {
       allowedList: [
         {
           // Match any request that starts 'https://YOUR_DOMAIN/api/v2/' (note the asterisk)
-          uri: 'https://api.peyya.io/v1/*',
+          uri: 'https://api.peyya.io/v1/developer',
           tokenOptions: {
             // The attached token should target this audience
-            audience: 'https://dev-ibqj5g5m.us.auth0.com/api/v2/',
+            audience: 'https://peyya.api',
 
             // The attached token should have these scopes
             scope: 'read:current_user read:email',
           },
         },
+        {
+          // Match any request that starts 'https://YOUR_DOMAIN/api/v2/' (note the asterisk)
+          uri: 'https://api.peyya.io/v1/developer/*',
+          tokenOptions: {
+            // The attached token should target this audience
+            audience: 'https://peyya.api',
+
+            // The attached token should have these scopes
+            scope: 'read:current_user read:email',
+          },
+        }
       ],
     },
   },
